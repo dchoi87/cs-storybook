40d277cbb3bcbfd6ebc0c7152737860d0aa5df1c
Daniel Choi <dchoi87@gmail.com>
2021-08-31 20:42:23 -0700

    Added  Theming

---
+++ .storybook/cs-theme.js
@@ -0,0 +1,36 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Cedars Sinai',
+    brandUrl: 'https://www.cedars-sinai.org/',
+    brandImage: 'https://www.cedars-sinai.org/content/dam/cedars-sinai/brand/Cedars-Sinai.png',
+
+    // colorPrimary: 'hotpink',
+    // colorSecondary: '#494949',
+
+    // ui
+    appBg: '#fafafa',
+    // appContentBg: 'purple',
+    // appBorderColor: 'green',
+    appBorderRadius: 7,
+
+    // typography
+    // fontBase: '"Roboto"',
+    // fontCode: 'Hack, monospace',
+
+    // text colors
+    textColor: '#494949',
+    // textInverseColor: 'pink',
+
+    // toolbar default and active colors
+    // barTextColor: '#494949',
+    // barSelectedColor: '#dc1e34',
+    // barBg: 'hotpink',
+
+    // form colors
+    // inputBg: '#fafafa',
+    // inputBorder: '#efefef',
+    // inputTextColor: 'orange',
+    inputBorderRadius: 7,
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import theme from './cs-theme';
+
+addons.setConfig({
+    theme: theme,
+});
